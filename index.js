f2a43e83fc444432a9b109057f59ca3c04a8ce57
Mohamed Ibrahim <mo7mad369@gmail.com>
2024-07-22 14:27:40 +0300

    done deploy

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const server = express();
-
-server.listen(3000);
-
-server.get("/", (req, res) =>
-  res.send(`node: ${process.version},<br /> it's working!!`)
-);
